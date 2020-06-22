c3f85f23285e1ffc37d9bad4ec8f852cbca48d4e
Ronald <ronald.drp11@gmail.com>
2020-06-22 02:42:10 -0300

    updated code to parse command line arguments to accept gpu training and grid search enable and now saves model output

--- amlsim_xgb.py
+++ amlsim_xgb.py
@@ -1,4 +1,5 @@
 import argparse
+import pickle
 
 import pandas as pd
 import xgboost as xgb
@@ -18,7 +19,7 @@ def read_data():
     return train_x, dev_x, test_x, train_y, dev_y, test_y
 
 
-def train_model(train_x, train_y, dev_x, dev_y, gpu=False):
+def search_hyperparams(train_x, train_y, dev_x, dev_y, gpu=False):
     params = {'objective': 'binary:logistic', 'n_estimators': 100, 'n_jobs': -1}
     if gpu:
         params.update({'tree_method': 'gpu_hist', 'gpu_id': 0})
@@ -42,7 +43,65 @@ def train_model(train_x, train_y, dev_x, dev_y, gpu=False):
 
     results_df.to_csv('data/results_grid_search_cv.csv', index=False)
 
+    with open('model/xgb_model.pickle', 'wb') as f:
+        pickle.dump(clf.best_estimator_)
+
+
+def f1_score_custom(y_pred, y_true):
+    y_true = y_true.get_label()
+    y_pred = y_pred > 0.5
+
+    return 'f1_err', 1 - f1_score(y_true, y_pred, average='binary')
+
+
+def train_model(train_x, train_y, dev_x, dev_y, gpu=False):
+    params = {'objective': 'binary:logistic', 'n_estimators': 300, 'n_jobs': -1}
+    if gpu:
+        params.update({'tree_method': 'gpu_hist', 'gpu_id': 0})
+    params.update({'gamma': 0, 'learning_rate': 0.5, 'reg_alpha': 0.5, 'reg_lambda': 0})
+
+    xgb_model = xgb.XGBClassifier(**params)
+
+    xgb_model.fit(train_x,
+                  train_y,
+                  eval_set=[(train_x, train_y), (dev_x, dev_y)],
+                  eval_metric=f1_score_custom)
+
+    y_pred = xgb_model.predict(test_x)
+
+    y_pred = y_pred > 0.5
+
+    print('f1_score_binary on test set: %f' % f1_score(test_y, y_pred, average='binary'))
+    print('f1_score_macro on test set: %f' % f1_score(test_y, y_pred, average='macro'))
+
+    with open('model/xgb_model.pickle', 'wb') as f:
+        pickle.dump(xgb_model, f)
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-s',
+                        '--search',
+                        action='store_true',
+                        default=False,
+                        help='Enable GridSearchCV mode')
+
+    parser.add_argument('-g',
+                        '--gpu',
+                        action='store_true',
+                        default=False,
+                        help='Enable GPU XGBoost training')
+
+    args = parser.parse_args()
+
+    return args
+
 
 if __name__ == "__main__":
+    args = arg_parser()
     train_x, dev_x, test_x, train_y, dev_y, test_y = read_data()
-    xgb_model = train_model(train_x, train_y, dev_x, dev_y, gpu=False)
+    if args.search:
+        search_hyperparams(train_x, train_y, dev_x, dev_y, gpu=args.gpu)
+    else:
+        train_model(train_x, train_y, dev_x, dev_y, gpu=args.gpu)
