531d133d5c8a80e318b228284aee3332ffc051be
Ronald <ronald.drp11@gmail.com>
2020-09-07 10:07:06 -0300

    run_all script messages for debug

--- run_all.sh
+++ run_all.sh
@@ -1,24 +1,40 @@
-python amlsim_process.py data/transactions.csv amlsim
-python amlsim_xgb.py -d amlsim -s > output/xgb_amlsim_cv.txt
-
+echo 'amlsim_sample xgboost data'
 python amlsim_process.py data/transactions_sample.csv amlsim_sample
+echo 'amlsim_sample xgboost train'
 python amlsim_xgb.py -d amlsim_sample -s > output/xgb_amlsim_sample_cv.txt
 
-python amlsim_process.py data/transactions_10k.csv amlsim_10k
-python amlsim_xgb.py -d amlsim_10k -s > output/xgb_amlsim_10k_cv.txt
-
-python amlsim_process.py data/transactions_oversample.csv amlsim_oversample
-python amlsim_xgb.py -d amlsim_oversample -s > output/xgb_amlsim_oversample_cv.txt
-
-python amlsim_process.py data/transactions.csv amlsim
-python amlsim_cat.py -d amlsim -s > output/cat_amlsim_cv.txt
-
+echo 'amlsim_sample catboost data'
 python amlsim_process.py data/transactions_sample.csv amlsim_sample
+echo 'amlsim_sample catboost train'
 python amlsim_cat.py -d amlsim_sample -s > output/cat_amlsim_sample_cv.txt
 
+echo 'amlsim xgboost data'
+python amlsim_process.py data/transactions.csv amlsim
+echo 'amlsim xgboost train'
+python amlsim_xgb.py -d amlsim -s > output/xgb_amlsim_cv.txt
+
+echo 'amlsim catboost data'
+python amlsim_process.py data/transactions.csv amlsim
+echo 'amlsim catboost train'
+python amlsim_cat.py -d amlsim -s > output/cat_amlsim_cv.txt
+
+echo 'amlsim_10k xgboost data'
 python amlsim_process.py data/transactions_10k.csv amlsim_10k
+echo 'amlsim_10k xgboost train'
+python amlsim_xgb.py -d amlsim_10k -s > output/xgb_amlsim_10k_cv.txt
+
+echo 'amlsim_10k catboost data'
+python amlsim_process.py data/transactions_10k.csv amlsim_10k
+echo 'amlsim_10k catboost train'
 python amlsim_cat.py -d amlsim_10k -s > output/cat_amlsim_10k_cv.txt
 
+echo 'amlsim_oversample xgboost data'
 python amlsim_process.py data/transactions_oversample.csv amlsim_oversample
+echo 'amlsim_oversample xgboost train'
+python amlsim_xgb.py -d amlsim_oversample -s > output/xgb_amlsim_oversample_cv.txt
+
+echo 'amlsim_oversample catboost data'
+python amlsim_process.py data/transactions_oversample.csv amlsim_oversample
+echo 'amlsim_oversample catboost train'
 python amlsim_cat.py -d amlsim_oversample -s > output/cat_amlsim_oversample_cv.txt
 
